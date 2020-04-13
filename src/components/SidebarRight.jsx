dd02976f7dd29fa0cf980ea4b37d283fe15170c1
Javier <jlopezcur@gmail.com>
2020-04-13 18:50:24 +0200

    Update SidebarRight.jsx

--- src/components/SidebarRight.jsx
+++ src/components/SidebarRight.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Tags from './sidebar/Tags';
-import Video from './sidebar/Video';
 
 const StyledSidebarRight = styled.div`
   grid-area: sidebarRight;
@@ -14,7 +13,6 @@ const StyledSidebarRight = styled.div`
 const SidebarRight = () => (
   <StyledSidebarRight>
     <Tags />
-    <Video />
   </StyledSidebarRight>
 );
 
